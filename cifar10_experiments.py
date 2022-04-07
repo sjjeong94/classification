864f0180031aa4bf587d2741f0039e195f65af58
sjjeong <sjjeong94@gmail.com>
2022-04-07 22:38:13 +0900

    exp 001

--- cifar10_experiments.py
+++ cifar10_experiments.py
@@ -144,6 +144,89 @@ def exp_000():
     train(epochs, net, train_loader, test_loader,
           device, criterion, optimizer, scheduler)
 
+def ConvBNReLU(in_ch, out_ch, k_size, stride=1, padding=0):
+    return nn.Sequential(
+        nn.Conv2d(in_ch, out_ch, k_size, stride, padding, bias=False),
+        nn.BatchNorm2d(out_ch),
+        nn.ReLU()
+    )
+
+class ResBlock(nn.Module):
+    def __init__(self, in_ch, out_ch, k_size, stride=1, padding=0):
+        super().__init__()
+        self.layers = nn.Sequential(
+            ConvBNReLU(in_ch, out_ch, k_size, stride, padding),
+            ConvBNReLU(in_ch, out_ch, k_size, stride, padding),
+        )
+    
+    def forward(self, x):
+        return self.layers(x) + x
+
+def exp_001():
+    set_seed(1234)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    root = './data'
+    batch_size = 100
+    T_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465),
+                             (0.2023, 0.1994, 0.2010)),
+    ])
+    T_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465),
+                             (0.2023, 0.1994, 0.2010)),
+    ])
+
+    load_dataset = datasets.CIFAR10
+
+    train_dataset = load_dataset(
+        root=root, train=True, transform=T_train, download=True)
+    test_dataset = load_dataset(
+        root=root, train=False, transform=T_test, download=False)
+
+    # Data Loader
+    train_loader = torch.utils.data.DataLoader(
+        dataset=train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(
+        dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+    net = nn.Sequential(
+        ConvBNReLU(3, 64, 3, 1, 1),
+        ConvBNReLU(64, 128, 3, 1, 1),
+        nn.MaxPool2d(2, 2),
+        ResBlock(128, 128, 3, 1, 1),
+        ConvBNReLU(128, 256, 3, 1, 1),
+        nn.MaxPool2d(2, 2),
+        ConvBNReLU(256, 512, 3, 1, 1),
+        nn.MaxPool2d(2, 2),
+        ResBlock(512, 512, 3, 1, 1),
+        nn.AdaptiveAvgPool2d((1, 1)),
+        nn.Flatten(),
+        nn.Linear(512, 10)
+    )
+    net = net.to(device)
+
+    epochs = 24
+    learning_rate = 0.1
+    momentum = 0.9
+    weight_decay = 5e-4
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(net.parameters(
+    ), lr=learning_rate, momentum=momentum, weight_decay=weight_decay)
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer,
+                                                    max_lr=learning_rate,
+                                                    epochs=epochs,
+                                                    steps_per_epoch=len(
+                                                        train_loader),
+                                                    anneal_strategy='linear')
+
+    train(epochs, net, train_loader, test_loader,
+          device, criterion, optimizer, scheduler)
 
 if __name__ == '__main__':
-    exp_000()
+    exp_001()
